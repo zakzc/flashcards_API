f33412b9bbecac22ee2cde2fa1e641f9b58d5b8e
Zak's MacBookpro <zakzangrando@gmail.com>
2020-10-02 20:31:28 +0200

    added routing error handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 // file imports
 const cardMan = require("./routes/cardMan");
 const userMan = require("./routes/userMan");
+// model imports
+const HttpError = require("./models/http_error");
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,6 +15,11 @@ app.use(bodyParser.json());
 app.use("/cardApi", cardMan);
 
 app.use("/userApi", userMan);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Route not found", 404);
+  throw error;
+});
 
 // Error handling
 app.use((error, req, res, next) => {
